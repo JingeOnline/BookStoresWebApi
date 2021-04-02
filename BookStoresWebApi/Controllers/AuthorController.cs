432118b8b1a62707e21ca8017866648194e857c5
Jinge Zhang <jinge_windows@hotmail.com>
2021-04-02 17:05:26 +1100

    添加项目文件。

---
+++ BookStoresWebApi/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using BookStoresWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Author> Get()
+        {
+            using (var context=new BookStoresDBContext())
+            {
+                //返回所有作者
+                return context.Authors.ToList();
+            }
+        }
+    }
+}
